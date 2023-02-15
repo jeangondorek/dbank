396792630b94bdfd01b76b6f602c81d8b876a74d
jeangondorek <jeangondorek73@gmail.com>
2023-02-15 10:58:08 -0300

    update

---
+++ src/components/IconText.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../scss/Institucional.scss";
+import {Container, Row, Col} from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlobe, faMobileAndroid, faMobileRetro, faShield } from '@fortawesome/free-solid-svg-icons';
+
+const IconText = ({icon, size}) => (
+    <Container>
+        <Row>
+            <Col>
+                <FontAwesomeIcon className="me-2 d-inline-block" icon={faMobileRetro} /> <p className="d-inline-block">Sem fila e sem burocracia</p><br/>
+                <FontAwesomeIcon className="me-2 d-inline-block" icon={faMobileAndroid} /> <p className="d-inline-block">Simples e prático</p><br/>
+                <FontAwesomeIcon className="me-2 d-inline-block" icon={faGlobe} /> <p className="d-inline-block">Abertura de conta 100% online</p><br/>
+                <FontAwesomeIcon className="me-2 d-inline-block" icon={faShield} /> <p className="d-inline-block">Transações mais seguras</p><br/>
+            </Col>
+        </Row>
+    </Container>    
+);
+
+export default IconText;
--- src/components/Institucional.js
+++ src/components/Institucional.js
@@ -1,25 +1,22 @@
 import React from "react";
 import "../scss/Institucional.scss";
 import {Container, Row, Col, Button} from "react-bootstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGlobe, faMobileAndroid, faMobileRetro, faShield } from '@fortawesome/free-solid-svg-icons';
+import IconText from "./IconText";
 
 const Institucional = () => (
-    <Container className="institucional">
-        <Row>
-            <Col xs={12} lg={5}/>
-            <Col xs={12} lg={7}>
-                <h3>Já nascemos digital</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue. Maecenas et porta magna. Nulla mattis tortor vitae metus vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur scelerisque placerat ultrices. Proin purus ante, congue vel arcu ac, porta condimentum mauris. Maecenas massa orci, fringilla eget fermentum nec, dignissim nec diam.</p>
-                <FontAwesomeIcon className="d-inline" icon={faMobileRetro}/><p className="d-inline">Sem fila e sem burocracia</p>
-                <FontAwesomeIcon icon={faMobileAndroid} />
-                <FontAwesomeIcon icon={faGlobe} />
-                <FontAwesomeIcon icon={faShield} />
-                <Button variant='outline-dark' className="d-block">Abra já sua conta</Button>
-            </Col>
-        </Row>
-
-    </Container>
+    <div className="institucional text-light py-5">
+        <Container>
+            <Row>
+                <Col xs={12} lg={5}/>
+                <Col xs={12} lg={7}>
+                    <h3>Já nascemos digital</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue. Maecenas et porta magna. Nulla mattis tortor vitae metus vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur scelerisque placerat ultrices. Proin purus ante, congue vel arcu ac, porta condimentum mauris. Maecenas massa orci, fringilla eget fermentum nec, dignissim nec diam.</p>
+                    <IconText/>
+                    <Button className="px-5 ms-3 d-block" variant='outline-light'>Abra já sua conta</Button>
+                </Col>
+            </Row>
+        </Container>
+    </div>
     
 );
 
